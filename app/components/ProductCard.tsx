f4324c10f3269f4828054b71df6cb0a70edf7f62
Prasanna Kumar <kumar22maran@gmail.com>
2023-09-22 21:16:09 +0530

    Initial Commit

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import AddtoCart from './AddtoCart';
+//import styles from './ProductCard.module.css';
+
+
+
+
+const ProductCard = () => {
+  return (
+    <div>
+        <AddtoCart/>
+    </div>
+  )
+}
+
+export default ProductCard 
